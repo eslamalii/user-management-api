a44ade3cd30595ee17c6dfbd513c131ebb548a68
eslamalii <preslamali@hotmail.com>
2025-02-03 16:29:22 +0200

    Remove Netlify deployment configuration and related dependencies

--- src/netlify/functions/server.ts
+++
@@ -1,9 +0,0 @@
-import serverless from 'serverless-http'
-import express from 'express'
-import app from '../../app'
-
-const server = express()
-
-server.use('/.netlify/functions/server', app)
-
-export const handler = serverless(server)
